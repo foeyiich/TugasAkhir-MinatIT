85b2f15bbe3c0fd6b4f81aa1cb01a0259e481aec
Foeyii <juan.fedrin@gmail.com>
2026-05-19 00:38:32 +0700

    -

--- views/login.php
+++ views/login.php
@@ -9,31 +9,31 @@ $error = $error ?? null;
     <title>Login SIM Debritto</title>
 </head>
 <body>
-    <h1>Login</h1>
+<h1>Login</h1>
 
-    <?php if ($error): ?>
-        <p style="color: red;"><?= htmlspecialchars($error) ?></p>
-    <?php endif; ?>
+<?php if ($error): ?>
+    <p style="color: red;"><?= htmlspecialchars($error) ?></p>
+<?php endif; ?>
 
-    <form method="POST" action="">
-        <div>
-            <label>Email</label>
-            <input type="email" name="email" required>
-        </div>
+<form method="POST" action="">
+    <div>
+        <label>Email</label>
+        <input type="email" name="email" required>
+    </div>
 
-        <div>
-            <label>Password</label>
-            <input type="password" name="password" required>
-        </div>
+    <div>
+        <label>Password</label>
+        <input type="password" name="password" required>
+    </div>
 
-        <div>
-            <label>
-                <input type="checkbox" name="remember_me" value="1">
-                Ingat saya
-            </label>
-        </div>
+    <div>
+        <label>
+            <input type="checkbox" name="remember_me" value="1">
+            Ingat saya
+        </label>
+    </div>
 
-        <button type="submit">Login</button>
-    </form>
+    <button type="submit">Login</button>
+</form>
 </body>
 </html>
